e6fb128cdb5a41ea1390babdb54537b9059c881f
Jasper Luong <jasperluong@Jaspers-MacBook-Air.local>
2022-02-06 00:03:41 -0800

    second commit

---
+++ utils/constants.js
@@ -0,0 +1,13 @@
+export const CLIENT_ID = "74c12fad7a004a90ac56df1fe7f03626";
+export const SCOPES = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+  "streaming",
+  "user-read-email",
+  "user-read-private"
+];
+export const REDIRECT_URI = "exp://10.128.150.105:19000";
+export const ALBUM_ID = "TODO: INSERT YOUR ALBUM ID HERE IF YOU ARE USING ONE";
